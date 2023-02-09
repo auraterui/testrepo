459e31984aed596da390e89679dc1749f29d9be6
auraterui <124839071+auraterui@users.noreply.github.com>
2023-02-09 13:52:01 +0800

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,14 @@
+import math #import math module
+
+pi = 3.141592653589793 #assign value of pi
+
+def print_volume(r):  
+
+    volume_of_sphere = 4/3 * pi * r**3 #apply formula to get final result
+
+    return volume_of_sphere
+
+print(' print_volume(5): volume of the sphere', print_volume(5))
+print('print_volume(10): volume of the sphere', print_volume(10))
+print('print_volume(15): volume of the sphere', print_volume(15))
+    
